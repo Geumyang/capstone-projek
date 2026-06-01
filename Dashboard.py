b40ec676859cc31b7a295ceee95998c0543e496e
Geumyang <melanisulistiawati@mail.ugm.ac.id>
2026-06-01 22:26:56 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -94,10 +94,7 @@ with st.sidebar:
     gender     = st.radio("Jenis kelamin:", ["Laki-laki", "Perempuan"])
     gender_int = 0 if gender == "Laki-laki" else 1
     usia       = st.slider("Usia (tahun):", 1, 80, 25)
-    if usia == 80:
-        usia_str = "80+"
-    else:
-        usia_str = f"{usia} tahun"
+    usia_str = "80+ tahun" if usia == 80 else f"{usia} tahun"
     kondisi    = "Normal"
     if gender == "Perempuan":
         kondisi = st.selectbox("Kondisi fisiologis:", [
